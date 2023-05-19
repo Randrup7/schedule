2d40d7bc090a99e01c5a6c2031af69309be3ed94
Randrup7 <68643421+Randrup7@users.noreply.github.com>
2023-05-19 14:20:57 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "schedule.h"
 
 int main()
-{   
+{
     schedule sched1{finDate(2022, 7, 20), finDate(2027, 5, 20), 4, 
                     std::unique_ptr<I_dayAdjustment>(new dayAdjustment::MF()),
                     std::unique_ptr<I_stub>(new stub::LongFinal()) };
