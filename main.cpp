79b689ba6b993654422c50ddb0def57ea913f427
Randrup7 <68643421+Randrup7@users.noreply.github.com>
2023-05-19 17:00:26 +0200

    added day_of_year method in finDate

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 
 int main()
 {
-    schedule sched1{finDate(2022, 7, 20), finDate(2027, 5, 20), 4, 
+/*    schedule sched1{finDate(2022, 7, 20), finDate(2027, 5, 20), 4, 
                     std::unique_ptr<I_dayAdjustment>(new dayAdjustment::MF()),
                     std::unique_ptr<I_stub>(new stub::LongFinal()) };
+*/
+
+    finDate dato(2023,5,19);
+    std::cout << dato.day_of_year() << '\n';
 
     return 0;
 }
