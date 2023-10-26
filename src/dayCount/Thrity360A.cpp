96557363a1930e7365e47371e24b073624eddd25
Randrup7 <68643421+Randrup7@users.noreply.github.com>
2023-10-26 22:09:41 +0200

    Fixed more daycounts

--- src/dayCount/Thrity360A.cpp
+++ src/dayCount/Thrity360A.cpp
@@ -6,7 +6,7 @@ double dayCount::Thirty360A::operator()(const finDate& startDate, const finDate&
     unsigned int d1 = std::min<unsigned int>(30, startDate.day());
     unsigned int d2 = endDate.day();
     
-    if (d2 == 31 && d1 > 29) { d1 = 30; }
+    if (d2 == 31 && d1 > 29) { d2 = 30; }
 
     return ((endDate.year() - startDate.year()) * 360 + 
             (endDate.month() - startDate.month()) * 30 + 
