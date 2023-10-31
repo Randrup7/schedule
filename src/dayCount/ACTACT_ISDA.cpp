ab72d8483e24175cf33daf84ac839ec40adf2b96
Randrup7 <68643421+Randrup7@users.noreply.github.com>
2023-10-31 22:19:54 +0100

    Not implemented yet

--- src/dayCount/ACTACT_ISDA.cpp
+++ src/dayCount/ACTACT_ISDA.cpp
@@ -4,6 +4,11 @@ double dayCount::ACTACT_ISDA::operator()(const finDate& startDate, const finDate
 {
     // if neither startdate or enddate is in leapyear and the difference between the dates is less than 4 years
     // then over 365
+    if (!startDate.is_leap_year() && !endDate.is_leap_year() && (endDate.year() - startDate.year()) < 4)
+    {
+        return (startDate - endDate) / 365.0;
+    }
+    
     
     
     return (startDate - endDate) / 365.0;
