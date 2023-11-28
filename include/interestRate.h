767bc4b95c045149d075e164f164fc2ac48fc37f
Randrup7 <68643421+Randrup7@users.noreply.github.com>
2023-11-28 21:51:52 +0100

    forwardRate, annuity and floatingPV function

--- include/interestRate.h
+++ include/interestRate.h
@@ -7,6 +7,7 @@
 #include "schedule/dayAdjustment.h"
 #include "calendars/calendars.h"
 #include "interpolate/interpolate.h"
+#include "schedule/stub.h"
 #include <memory>
 
 enum class compounding
@@ -40,10 +41,38 @@ public:
 double discountFactor(const interestRate& rate, double t);
 double discountFactor(const interestRate& rate, double t, compounding comp);
 
+// Calculate forward rate between start and maturity as per anchor
 double forwardRate(const finDate& anchor, const finDate& start, const finDate& maturity, 
                     const std::vector<std::pair<finDate, interestRate>>& curve, 
                     std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
-                    std::shared_ptr<I_dayCount> daycount); //, std::shared_ptr<I_dayAdjustment> dayAdjustment);
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment);
 
+// forward rate with interval as start and maturity input
+double forwardRate(const finDate& anchor, const interval& start, const interval& maturity, 
+                    const std::vector<std::pair<finDate, interestRate>>& curve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_calendar> Calendar);
+
+// Calculate annuity between start and maturity as per anchor
+double annuity(const finDate& anchor, const finDate& start, const finDate& maturity,
+                const interval& freq, const std::vector<std::pair<finDate, interestRate>>& discCurve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_stub> stub, std::shared_ptr<I_calendar> Calendar);
+
+// Annuity with interval as start and maturity input
+double annuity(const finDate& anchor, const interval& start, const interval& maturity,
+                const interval& freq, const std::vector<std::pair<finDate, interestRate>>& discCurve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_stub> stub, std::shared_ptr<I_calendar> Calendar);
+
+double floatingPV(const finDate& anchor, const finDate& start, const finDate& maturity,
+                const interval& freq, const std::vector<std::pair<finDate, interestRate>>& curve, 
+                const std::vector<std::pair<finDate, interestRate>>& discCurve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_stub> stub, std::shared_ptr<I_calendar> Calendar);
 
 #endif
--- src/interestRate.cpp
+++ src/interestRate.cpp
@@ -1,4 +1,5 @@
 #include "interestRate.h"
+#include "schedule.h"
 #include <cmath>
 
 interestRate::interestRate() : m_comp{ compounding::Continuous } {}
@@ -66,15 +67,15 @@ double discountFactor(const interestRate& rate, double t, compounding comp)
 double forwardRate(const finDate& anchor, const finDate& start, const finDate& maturity, 
                     const std::vector<std::pair<finDate, interestRate>>& curve, 
                     std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
-                    std::shared_ptr<I_dayCount> daycount) //, std::shared_ptr<I_dayAdjustment> dayAdjustment)
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment)
 {
     if (start >= maturity)
     {
-        throw std::invalid_argument( "forwardRate constructor: start date larger or equal to maturity date." );
+        throw std::invalid_argument( "forwardRate: start date larger or equal to maturity date." );
     }
     if (anchor > start)
     {
-        throw std::invalid_argument( "forwardRate constructor: start date less than anchor date." );
+        throw std::invalid_argument( "forwardRate: start date less than anchor date." );
     }
 
     // Assumption that compound convention is the same for all rates
@@ -84,7 +85,117 @@ double forwardRate(const finDate& anchor, const finDate& start, const finDate& m
     return (ps / pe - 1) / daycount->operator()(start, maturity);
 }
 
+double forwardRate(const finDate& anchor, const interval& start, const interval& maturity, 
+                    const std::vector<std::pair<finDate, interestRate>>& curve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment, 
+                    std::shared_ptr<I_calendar> calendar)
+{
+    if (maturity.isZero())
+    {
+        throw std::invalid_argument( "forwardRate: maturity cannot be zero interval." );
+    }
 
+    finDate startDate = anchor + start;
+    finDate maturityDate = startDate + maturity;
+
+    dayAdjustment->adjustDate(startDate, calendar);
+    dayAdjustment->adjustDate(maturityDate, calendar);
+
+    return forwardRate(anchor, startDate, maturityDate, curve, interpolate, daycount, dayAdjustment);
+}
+
+
+double annuity(const finDate& anchor, const finDate& start, const finDate& maturity,
+                const interval& freq, const std::vector<std::pair<finDate, interestRate>>& discCurve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_stub> stub, std::shared_ptr<I_calendar> Calendar)
+{
+    if (start >= maturity)
+    {
+        throw std::invalid_argument( "Annuity: start date larger or equal to maturity date." );
+    }
+    if (anchor > start)
+    {
+        throw std::invalid_argument( "Annuity: start date less than anchor date." );
+    }
+ 
+    schedule sched{start, maturity, freq, dayAdjustment, stub, Calendar};
+
+    double annuity = 0;
+    for (size_t i = 1; i < sched.m_paymentDates.size(); i++)
+    {
+        annuity += discountFactor(interpolate->operator()(discCurve, sched.m_paymentDates[i]), 
+                    (sched.m_paymentDates[i] - anchor) / 365.0, discCurve[0].second.m_comp) * 
+                    daycount->operator()(sched.m_paymentDates[i - 1], sched.m_paymentDates[i]);;
+    }
+    
+    return annuity;
+}
+
+double annuity(const finDate& anchor, const interval& start, const interval& maturity,
+                const interval& freq, const std::vector<std::pair<finDate, interestRate>>& discCurve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_stub> stub, std::shared_ptr<I_calendar> Calendar)
+{
+    if (maturity.isZero())
+    {
+        throw std::invalid_argument( "Annuity: maturity cannot be zero interval." );
+    }
+
+    finDate startDate = anchor + start;
+    finDate maturityDate = startDate + maturity;
+ 
+    return annuity(anchor, startDate, maturityDate, freq, discCurve, interpolate, daycount, dayAdjustment, stub, Calendar);
+}
+
+double floatingPV(const finDate& anchor, const finDate& start, const finDate& maturity,
+                const interval& freq, const std::vector<std::pair<finDate, interestRate>>& curve, 
+                const std::vector<std::pair<finDate, interestRate>>& discCurve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_stub> stub, std::shared_ptr<I_calendar> Calendar)
+{
+    if (start >= maturity)
+    {
+        throw std::invalid_argument( "floatingPV: start date larger or equal to maturity date." );
+    }
+    if (anchor > start)
+    {
+        throw std::invalid_argument( "floatingPV: start date less than anchor date." );
+    }
+
+    schedule sched{start, maturity, freq, dayAdjustment, stub, Calendar};
+    
+    double PV = 0;
+    for (size_t i = 1; i < sched.m_paymentDates.size(); i++)
+    {
+        PV += forwardRate(anchor, sched.m_paymentDates[i - 1], sched.m_paymentDates[i], curve, interpolate, daycount, dayAdjustment) *
+                discountFactor(interpolate->operator()(discCurve, sched.m_paymentDates[i]), (sched.m_paymentDates[i] - anchor) / 365.0, discCurve[0].second.m_comp) * 
+                daycount->operator()(sched.m_paymentDates[i - 1], sched.m_paymentDates[i]);;
+    }
+    
+    return PV;
+}
+
+double annuity(const finDate& anchor, const interval& start, const interval& maturity,
+                const interval& freq, const std::vector<std::pair<finDate, interestRate>>& discCurve, 
+                    std::shared_ptr<I_interpolate<finDate, interestRate>> interpolate,
+                    std::shared_ptr<I_dayCount> daycount, std::shared_ptr<I_dayAdjustment> dayAdjustment,
+                    std::shared_ptr<I_stub> stub, std::shared_ptr<I_calendar> Calendar)
+{
+    if (maturity.isZero())
+    {
+        throw std::invalid_argument( "Annuity: maturity cannot be zero interval." );
+    }
+
+    finDate startDate = anchor + start;
+    finDate maturityDate = startDate + maturity;
+ 
+    return annuity(anchor, startDate, maturityDate, freq, discCurve, interpolate, daycount, dayAdjustment, stub, Calendar);
+}
 
 std::ostream& operator<<(std::ostream& out, const interestRate& rhs)
 {
